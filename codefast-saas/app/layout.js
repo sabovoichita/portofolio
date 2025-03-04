82d5a4a8b111890e7c5c0bee33ddfc199b4963f3
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-04 16:47:13 +0100

    reconfigure daisyUi to work

--- codefast-saas/app/layout.js
+++ codefast-saas/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="dark">
+      <body className={`${inter.className} min-h-screen`}>{children}</body>
     </html>
   );
 }
