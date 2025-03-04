a3d2d93a8b450c352c91fe2fd93974e44061bda0
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-04 17:44:44 +0100

    modify content

--- codefast-saas/app/page.js
+++ codefast-saas/app/page.js
@@ -3,13 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Hello DaisyUI!</h2>
-          <p>If you see this, DaisyUI is working.</p>
-          <p className="text-red-500">Is this text red?</p>
-          <button className="btn btn-primary">Start now</button>
-        </div>
+      <h1>Collect Costumers to build better products</h1>
+      <div>
+        Create a feedback board in minutes, prioritize features, and build
+        products your customers will love.
       </div>
     </main>
   );
