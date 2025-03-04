dfe21feef70bf3818ad2cb5d6e3948626cca39df
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-04 15:33:53 +0100

    install next.js and npm i daisyui

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Inter } from "@next/font/google";
+
 let likeCounter = 0;
 const likeButton = document.getElementById("likeButton");
 //a function that increments  the number of likes
@@ -7,7 +9,7 @@ const addLike = (incrementBy) => {
   //show number of likes on the button, like this (3) Like 👍
   likeButton.innerText = `(${likeCounter}) Like 👍`;
 
-  console.log(likeCounter === 10);
+  //   console.log(likeCounter === 10);
 
   if (likeCounter === 10) {
     alert(" 🎉Congrats!");
