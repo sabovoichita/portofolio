6eda20a06163680900e22148303b7576324f9a4b
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-07 16:23:14 +0100

    push changes to create workflow

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  preload: true,
+});
 
 export const metadata = {
   title: "Voichita's Portofolio",
---
+++ auth.js
@@ -0,0 +1,15 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+};
+
+export default authOptions;
