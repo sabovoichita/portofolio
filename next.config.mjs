ece03aa9af78650114dac86a1427e0506071c80d
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-07 17:20:16 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/portofolio",
-  assetPrefix: "/portofolio/",
-  trailingSlash: true,
 };
 
 export default nextConfig;
